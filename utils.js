f388b6a4057c0f06568e8a00c7aaf6fbfe285e16
LyNnJeR31 <943280824@qq.com>
2020-07-19 23:14:24 +0800

    apply

--- utils.js
+++ utils.js
@@ -10,3 +10,20 @@ Function.prototype.myCall = function(context) {
   delete context.fn;
   return result;
 }
+
+// apply
+Function.prototype.myApply = function(context) {
+  if(typeof this !== 'function') {
+    throw new TypeError('not funciton')
+  }
+  context = context || window;
+  context.fn = this;
+  var result = null;
+  if(arguments[1]) {
+    result = context.fn(...arguments);
+  }else {
+    result = context.fn();
+  }
+  delete context.fn();
+  return result;
+}
